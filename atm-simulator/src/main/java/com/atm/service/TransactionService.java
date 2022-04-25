ef74b949b51bb2ebc401a8bd6c9cba927109d667
Dhruv KP <58841359+dhruvkp1737@users.noreply.github.com>
2022-04-25 23:22:26 +0530

    Add files via upload

---
+++ atm-simulator/src/main/java/com/atm/service/TransactionService.java
@@ -0,0 +1,28 @@
+package com.atm.service;
+
+import com.atm.dao.Transaction;
+import com.atm.dao.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The TransactionService class provides business logic for an account
+ */
+@Service
+public class TransactionService {
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    /**
+     * Function to save a transaction to the repository
+     * @param transaction
+     */
+    public void save(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    public Transaction getTransaction() {
+        Transaction t = transactionRepository.findTopByOrderByTransactionIdDesc();
+        return t;
+    }
+}
